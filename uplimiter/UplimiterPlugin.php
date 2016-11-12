798470f37b729a3901dd85d9a44ea7c98b0a1fb3
Aaron Bushnell <aaron.bushnell@gmail.com>
2016-11-12 00:38:14 -0500

    Update UplimiterPlugin.php

--- uplimiter/UplimiterPlugin.php
+++ uplimiter/UplimiterPlugin.php
@@ -4,16 +4,6 @@
  *
  * Allows you to define the maximum upload size per user group
  *
- * --snip--
- * Craft plugins are very much like little applications in and of themselves. We’ve made it as simple as we can,
- * but the training wheels are off. A little prior knowledge is going to be required to write a plugin.
- *
- * For the purposes of the plugin docs, we’re going to assume that you know PHP and SQL, as well as some semi-
- * advanced concepts like object-oriented programming and PHP namespaces.
- *
- * https://craftcms.com/docs/plugins/introduction
- * --snip--
- *
  * @author    TrendyMinds
  * @copyright Copyright (c) 2016 TrendyMinds
  * @link      http://trendyminds.com
